0d359b011d545052714ccc18d67b0a23c5d0f6cb
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-03-29 11:48:13 +0530

    commit

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const { CORS, PORT } = process.env;
 
 app.get("/", (_, res) => res.send("Satyam backend is running!"));
 
-app.use(cors({ origin: CORS, maxAge: 600, methods: ["GET", "POST", "PUT", "DELETE"] }));
+app.use(cors({ origin: [CORS], maxAge: 600, methods: ["GET", "POST", "PUT", "DELETE"] }));
 app.use(express.json());
 app.use(compression());
 
