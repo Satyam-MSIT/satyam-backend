bd38ca4ce9072d93da057edd14d72a9bc2ad8f5a
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-04-20 22:04:26 +0530

    commit

--- src/routes/volume.ts
+++ src/routes/volume.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { volumeSchema } from "../schemas/volume";
 import Volume from "../models/Volume";
+import Journal from "../models/Journal";
 import { getLatestVolume } from "../modules/utilities";
 import Newsletter from "../models/Newsletter";
 import { usePromises } from "../modules/promise";
@@ -9,11 +10,33 @@ import { generateMessage, sendMail } from "../modules/nodemailer";
 
 const router = Router();
 
+router.get("/all", async (req, res) => {
+  try {
+    const volumes = await Volume.find();
+    res.json({ success: true, volumes });
+  } catch {
+    res.status(500).json({ success: false, error: "Uh Oh, Something went wrong!" });
+  }
+});
+
+router.get("/latest", async (req, res) => {
+  try {
+    const { number, title, description, keywords } = (await Volume.findOne().sort("-number"))!;
+    const journals = await Journal.find({ journal_id: { $regex: `^\d{2}${number}` } });
+    res.json({ success: true, title, description, keywords, journals });
+  } catch {
+    res.status(500).json({ success: false, error: "Uh Oh, Something went wrong!" });
+  }
+});
+
 router.post("/call", async (req, res) => {
   try {
-    const { title, description, keywords, acceptanceTill, publishDate, acceptancePing, reviewPing } = await volumeSchema.parseAsync(req.body);
-    const volume = await getLatestVolume();
-    await Volume.create({ number: volume.number! + 1, title, description, keywords, acceptanceTill, publishDate, acceptancePing, reviewPing });
+    let { number, title, description, keywords, acceptanceTill, publishDate, acceptancePing, reviewPing } = await volumeSchema.parseAsync(req.body);
+    if (!number) {
+      const volume = await getLatestVolume();
+      number = volume.number! + 1;
+    }
+    await Volume.create({ number, title, description, keywords, acceptanceTill, publishDate, acceptancePing, reviewPing });
     const newsletters = await Newsletter.find();
     await usePromises(newsletters.map((user) => sendMail(generateMessage(user, "call"))));
     res.json({ success: true, msg: "Call for papers created successfully!" });
--- src/schemas/volume.ts
+++ src/schemas/volume.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const volumeSchema = z.object({
+  number: z.number().gte(1).lte(99).optional(),
   title: z.string(),
   description: z.string(),
   keywords: z.string().array(),
